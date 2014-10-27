61f5b3359b989a30f01ef094164ab92436e889ec
kelvin Atatawura <kelvin a@gmail.com>
2014-10-27 02:52:22 +0000

    amazing framework for wordpress theme development

---
+++ content-quote.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * content.php
+ *
+ * The default template for displaying posts with the Quote post format.
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<!-- Article content -->
+	<div class="entry-content">
+		<?php
+				the_content( __( 'Continue reading &rarr;', 'alpha' ) );
+
+				wp_link_pages();
+		?>
+	</div> <!-- end entry-content -->
+
+	<!-- Article footer -->
+	<footer class="entry-footer">
+		<p class="entry-meta">
+			<?php
+				// Display the meta information
+				awesome_post_meta();
+			?>
+		</p>
+	</footer> <!-- end entry-footer -->
+</article>
